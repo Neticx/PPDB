5ab36d7bc91ce9a1120a6b90fb43b2c5088f8182
Neticx <neticxploit@gmail.com>
2018-01-09 13:19:05 +0700

    delete sql

--- application/views/newui/formulir.php
+++ application/views/newui/formulir.php
@@ -29,24 +29,67 @@
                         </div>
                         <div class="body">
                             <form id="wizard_with_validation" method="POST">
-                                <h3>Account Information</h3>
+                                <h3>Data Siswa</h3>
                                 <fieldset>
-                                    <div class="form-group form-float">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="username" required>
-                                            <label class="form-label">Username*</label>
+                                    <div class="row clearfix">
+                                        <div class="col-md-6 col-sm-12 col-md-6">
+                                            <div class="form-group form-float">
+                                             <div class="form-line">
+                                                   <input type="text" name="name" class="form-control" required>
+                                                   <label class="form-label">Nama Lengkap*</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-sm-12 col-md-6">
+                                            <div class="form-group form-float">
+                                                <div class="form-line">
+                                                   <input type="text" name="surname" class="form-control" required>
+                                                   <label class="form-label">Nama Panggilan*</label>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="form-group form-float">
-                                        <div class="form-line">
-                                            <input type="password" class="form-control" name="password" id="password" required>
-                                            <label class="form-label">Password*</label>
+
+                                    <div class="row clearfix">
+                                        <div class="col-md-6">
+                                            <label>Jenis Kelamin *</label>
+                                            <div class="demo-radio-button">
+                                                <input name="kelamin" type="radio" id="radio_30" class="with-gap radio-col-green" checked />
+                                                <label for="radio_30">Laki Laki</label>
+                                                <input name="kelamin" type="radio" id="radio_31" class="with-gap radio-col-pink" />
+                                                <label for="radio_31">Perempuan</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label>Agama *</label>
+                                            <div class="demo-radio-button">
+                                                <input name="agama" type="radio" id="agama1" class="with-gap radio-col-green" checked />
+                                                <label for="agama1">Islam</label>
+                                                <input name="agama" type="radio" id="agama2" class="with-gap radio-col-green" />
+                                                <label for="agama2">Kristen</label>
+                                                <input name="agama" type="radio" id="agama3" class="with-gap radio-col-green" />
+                                                <label for="agama3">Hindu</label>
+                                                <input name="agama" type="radio" id="agama4" class="with-gap radio-col-green" />
+                                                <label for="agama4">Budha</label>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="form-group form-float">
-                                        <div class="form-line">
-                                            <input type="password" class="form-control" name="confirm" required>
-                                            <label class="form-label">Confirm Password*</label>
+                                    <div class="row clearfix">
+                                        <div class="col-md-6">
+                                            <div class="form-group form-float">
+                                                <div class="form-line">
+                                                   <input type="text" name="ttl" class="form-control" required>
+                                                   <label class="form-label">Tempat Tanggal Lahir*</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group form-float">
+                                                <div class="form-line">
+                                                   <input type="text" name="sekolah" class="form-control" required>
+                                                   <label class="form-label">Sekolah Asal*</label>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </fieldset>
@@ -61,7 +104,7 @@
                                     </div>
                                     <div class="form-group form-float">
                                         <div class="form-line">
-                                            <input type="text" name="surname" class="form-control" required>
+                                            <input type="text" name="surnames" class="form-control" required>
                                             <label class="form-label">Last Name*</label>
                                         </div>
                                     </div>
